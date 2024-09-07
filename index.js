8862598ce5c3b7db1a445062af952e930b88c77d
Victor Greiveldinger <victorgreiveldinger@gmail.com>
2024-09-08 00:23:25 +0200

    WYP: adding dragable slider and dynamic switch on hover

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ for (let i = 0; i < images.length; i++) {
     dynamic[0].src = images[i].src;
   });
 }
+
+// Gestion du premier clique pour "scroller" les images
+const track = document.getElementById("dragable_content");
+window.onmousedown = function (e) {
+  track.dataset.mouseDownAt = e.clientY;
+};
+
+window.onmouseup = function (e) {
+  track.dataset.mouseDownAt = 0;
+  track.dataset.prevPercentage = track.dataset.percentage;
+};
+
+window.onmousemove = function (e) {
+  if (track.dataset.mouseDownAt === "0") return;
+  const delta = parseFloat(track.dataset.mouseDownAt) - e.clientY,
+    maxDelta = window.innerHeight / 2;
+
+  const percentage = (delta / maxDelta) * -100,
+    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+  console.log(nextPercentage);
+  track.dataset.percentage = nextPercentage;
+  track.style.transform = `translate(0%, ${nextPercentage}%)`;
+};
